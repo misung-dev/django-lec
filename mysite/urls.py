048133baf1bdb5a7318736645d2f26b895129e1f
misung.dev <misung.dev@gmail.com>
2023-07-24 02:34:08 +0900

    Fix: 3주차 header, footer 템플릿화, product 상세페이지 제작 및 미리보기 기능 적용

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # 뷰 함수를 views.py 파일에 정의하고 index로 매핑
-    # 다른 URL 패턴과 뷰 함수를 추가로 등록 가능
+    path('', views.index),
+    path('<int:content_id>/', views.detail, name='detail'),
 ]
